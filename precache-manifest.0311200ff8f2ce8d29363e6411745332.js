1a37c6bbe64f15c58e2a37a06781adf6b1ffccb9
Faisal Haque Bappy <hbfaisal66@gmail.com>
2019-11-26 23:05:41 +0600

    Updates

---
+++ precache-manifest.0311200ff8f2ce8d29363e6411745332.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4e96b5cb13084bfa2e090b3aaaec13c",
+    "url": "/time-calculator/index.html"
+  },
+  {
+    "revision": "15d3b84facf4d267ecec",
+    "url": "/time-calculator/static/css/main.14fd1a6d.chunk.css"
+  },
+  {
+    "revision": "04d36b64d4f57ea35ef0",
+    "url": "/time-calculator/static/js/2.20fe4424.chunk.js"
+  },
+  {
+    "revision": "15d3b84facf4d267ecec",
+    "url": "/time-calculator/static/js/main.d836f4e2.chunk.js"
+  },
+  {
+    "revision": "86cafc14e0f9e82f0100",
+    "url": "/time-calculator/static/js/runtime-main.5275a982.js"
+  }
+]);
